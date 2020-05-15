b41fffa1458e33f426c7cbc7574943ed07dcc576
Thomas <tg.59290@gmail.com>
2020-05-15 09:45:45 +0200

    refactor and add command choice for user

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-// Variables
-let score = 7;
-
-
 // Function
+function askCommand() {
+  const commands = ["j", "r", "q"];
+  let command = "";
+  while(!commands.includes(command)) {
+    command = prompt("Que souhaitez-vous faire : j : jouer, r : règles, q : quitter");
+  }
+  return command;
+}
+
 function getRandomWord() {
   const words = ["banane", "terre", "voiture", "jouet"];
   let index = Math.floor(Math.random() * Math.floor(words.length));
@@ -17,7 +22,15 @@ function getEmptyArray(word) {
   return result;
 }
 
-function updateGuess(letter) {
+function askLetter(guess, score) {
+  let letter;
+  do {
+    letter = prompt(score + " coups restant, mot à deviner : \n" + guess.join(' '));
+  } while (letter.length !== 1);
+  return letter;
+}
+
+function updateGuess(word, guess, letter) {
   for (let i = 0; i < word.length; i++) {
     if(word[i] === letter) {
       guess[i] = letter
@@ -25,22 +38,42 @@ function updateGuess(letter) {
   }
 }
 
-// Logic
+function play() {
+  let score = 7;
+  let word = getRandomWord();
+  let guess = getEmptyArray(word);
 
-let word = getRandomWord();
-let guess = getEmptyArray(word);
+  while (guess.includes("_") && score > 0) {
+    let letter = askLetter(guess, score);
 
-while (guess.includes("_")) {
-  let letter = prompt(score + " coups restant, mot à deviner : \n" + guess.join(' '));
-
-  if (word.includes(letter)) {
-    updateGuess(letter);
+    if (word.includes(letter)) {
+      updateGuess(word, guess, letter);
+    }
+    else {
+      score --;
+    }
+  }
+  if(score === 0) {
+    alert("Vous avez été pendu !");
   }
   else {
-    score --;
-    if(score === 0) {
-      alert("Vous avez été pendu !");
-      break;
-    }
+    alert("Bravo, le mot était : " + word);
+  }
+}
+
+
+// Logic
+
+while(true) {
+  let command = askCommand();
+  if(command === "j") {
+    play();
+  }
+  else if(command === "r") {
+    alert("Voici les règles du jeu");
+  }
+  else {
+    alert("A bientôt");
+    break;
   }
 }
